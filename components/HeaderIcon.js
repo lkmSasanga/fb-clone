38bd08140570015c604ef823be7c5d893810dc28
lkmSasanga <lkmsasanga@gmail.com>
2021-07-08 18:31:31 +0530

    center header added

---
+++ components/HeaderIcon.js
@@ -0,0 +1,12 @@
+function HeaderIcon({ Icon }) {
+  return (
+    <div
+      className="flex items-center cursor-pointer md:px-10 sm:h-14
+     md:hover:bg-gray-100 rounded-xl active: border-b-2 active:border-blue-500"
+    >
+      <Icon className="h-5 hover:text-blue-500" />
+    </div>
+  );
+}
+
+export default HeaderIcon;
